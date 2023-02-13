5c64d7c5f1643c4149d214ab594d540784d84ba3
amanShouts <aman.shouts@gmail.com>
2023-02-13 13:37:09 +0530

    added final changes

--- sugar-cosmetics-clone/sugar/src/App.jsx
+++ sugar-cosmetics-clone/sugar/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import AllRoutes from "./Components/AllRoutes/AllRoutes"
+import AllRoutes from "./Components/AllRoutes/AllRoutes";
+import { Navbar } from "./Components/Pages/aman/Navbar";
+import { Footer } from "./Components/Pages/aman/Footer";
 function App() {
-
-  return (
-    <div className="App">
-      <AllRoutes/>
-    </div>
-  );
-
+    return (
+        <div className="App">
+            <Navbar />
+            <AllRoutes />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
