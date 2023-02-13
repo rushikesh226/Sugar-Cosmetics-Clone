c9f372fa04668560b1f2059c28b4f3a8ef933dfe
amanShouts <aman.shouts@gmail.com>
2023-02-13 15:25:53 +0530

    akshata up to date

--- sugar-cosmetics-clone/sugar/src/Components/AllRoutes/AllRoutes.jsx
+++ sugar-cosmetics-clone/sugar/src/Components/AllRoutes/AllRoutes.jsx
@@ -6,6 +6,8 @@ import IndividualProductPage from "../Pages/aman/IndividualProductPage";
 import { PaymentPage } from "../Pages/aman/PaymentPage";
 import NotFoundPage from "../Pages/Product/NotFoundPage";
 import { CartPage } from "../Pages/aman/CartPage";
+import Log from "../Pages/LoginSignin/Login";
+import Otp from "../Pages/LoginSignin/Otp";
 function AllRoutes() {
     return (
         <Routes>
