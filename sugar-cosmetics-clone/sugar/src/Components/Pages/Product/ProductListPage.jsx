6ba4ccecc205226e2722373fd6eebe23a5150db5
rushikesh226 <rushikeshmali226@gmail.com>
2023-02-14 16:17:15 +0530

    json server

--- sugar-cosmetics-clone/sugar/src/Components/Pages/Product/ProductListPage.jsx
+++ sugar-cosmetics-clone/sugar/src/Components/Pages/Product/ProductListPage.jsx
@@ -4,17 +4,18 @@ import { useState } from "react";
 import { SortContext } from "../../Context/SortContext";
 import Filters from "./Filters";
 import ProductCard from "./ProductCard";
-
+import jsonData from "../../../db.json";
 function ProductListPage() {
+  console.log(jsonData);
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const { sort } = useContext(SortContext);
   useEffect(() => {
     getData();
   }, [sort]);
   const getData = () => {
     setIsLoading(true);
-    fetch(`http://localhost:3001/${sort}`)
+    fetch(`https://sugar-cosmetics-server-json.onrender.com/${sort}`)
       .then((res) => res.json())
       .then((data) => {
         setData(data);
@@ -22,41 +23,38 @@ function ProductListPage() {
       });
   };
   return (
-    <>
-
-      <Box
-        display={"flex"}
-        justifyContent="space-between"
-        gap={"2%"}
-        backgroundColor="#F2F2F2"
-      >
-        <Box width={"20%"} backgroundColor="white" height={"400px"}>
-          <Filters />
-        </Box>
-        <Box
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3,1fr)",
-            width: "80%",
-            marginBottom: "50px",
-          }}
-        >
-          {data.map((product, i) => {
-            return (
-              <ProductCard
-                key={i}
-                image={product.image}
-                title={product.title}
-                price={product.price}
-                rating={product.rating}
-                reviews={product.reviews}
-                shades={product.shades}
-              />
-            );
-          })}
-        </Box>
+    <Box
+      display={"flex"}
+      justifyContent="space-between"
+      gap={"2%"}
+      backgroundColor="#F2F2F2"
+    >
+      <Box width={"20%"} backgroundColor="white" height={"400px"}>
+        <Filters />
       </Box>
-    </>
+      <Box
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          width: "80%",
+          marginBottom: "50px",
+        }}
+      >
+        {data.map((product, i) => {
+          return (
+            <ProductCard
+              key={i}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              rating={product.rating}
+              reviews={product.reviews}
+              shades={product.shades}
+            />
+          );
+        })}
+      </Box>
+    </Box>
   );
 }
 
