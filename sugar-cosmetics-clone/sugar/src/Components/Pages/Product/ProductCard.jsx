3d660c4553139a343c9fc426e601a90307d7dacf
rushikesh226 <rushikeshmali226@gmail.com>
2023-02-20 09:04:23 +0530

    wishlist done

--- sugar-cosmetics-clone/sugar/src/Components/Pages/Product/ProductCard.jsx
+++ sugar-cosmetics-clone/sugar/src/Components/Pages/Product/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, useToast } from "@chakra-ui/react";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../../Redux/action";
+import { addToCart, addToWishlist } from "../../Redux/action";
 import { useNavigate } from "react-router-dom";
 function ProductCard({ image, title, price, rating, reviews, shades }) {
   const navigate = useNavigate();
@@ -28,7 +28,16 @@ function ProductCard({ image, title, price, rating, reviews, shades }) {
     setCartText("ADDED TO CART");
     setCartColor("teal");
   };
-
+  const productAddToWishlist = () => {
+    dispatch(addToWishlist(cartObj));
+    toast({
+      title: "Item Added To Wishlist",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+    
+  };
   return (
     <Box
       style={{
@@ -105,21 +114,23 @@ function ProductCard({ image, title, price, rating, reviews, shades }) {
             margin: "auto",
           }}
         >
-          <svg
-            viewBox="0 0 32 33"
-            width="1em"
-            height="1em"
-            style={{ fontSize: "45px", verticalAlign: "middle" }}
-          >
-            <path
-              d="M9 .5h14c4.7 0 8.5 3.8 8.5 8.5v14c0 4.7-3.8 8.5-8.5 8.5H9C4.3 31.5.5 27.7.5 23V9C.5 4.3 4.3.5 9 .5z"
-              style={{ fill: "rgb(255, 255, 255)", stroke: "rgb(0, 0, 0)" }}
-            ></path>
-            <path
-              d="M22.3 11.8c.4.4.7.9.9 1.4.2.5.3 1.1.3 1.6 0 .6-.1 1.1-.3 1.6-.2.5-.5 1-.9 1.4l-5.6 5.6s-.1 0-.1.1h-.2s-.1 0-.1-.1l-5.6-5.6C10 17 9.5 16 9.5 14.9c0-1.1.3-2.1 1-2.9.7-.8 1.7-1.3 2.7-1.5 1.1-.1 2.1.2 3 .8l.3.2.3-.2c.8-.6 1.8-.9 2.8-.8 1.1.1 2 .5 2.7 1.3z"
-              style={{ fill: "none", stroke: "rgb(0, 0, 0)" }}
-            ></path>
-          </svg>
+          <button onClick={productAddToWishlist}>
+            <svg
+              viewBox="0 0 32 33"
+              width="1em"
+              height="1em"
+              style={{ fontSize: "45px", verticalAlign: "middle" }}
+            >
+              <path
+                d="M9 .5h14c4.7 0 8.5 3.8 8.5 8.5v14c0 4.7-3.8 8.5-8.5 8.5H9C4.3 31.5.5 27.7.5 23V9C.5 4.3 4.3.5 9 .5z"
+                style={{ fill: "rgb(255, 255, 255)", stroke: "rgb(0, 0, 0)" }}
+              ></path>
+              <path
+                d="M22.3 11.8c.4.4.7.9.9 1.4.2.5.3 1.1.3 1.6 0 .6-.1 1.1-.3 1.6-.2.5-.5 1-.9 1.4l-5.6 5.6s-.1 0-.1.1h-.2s-.1 0-.1-.1l-5.6-5.6C10 17 9.5 16 9.5 14.9c0-1.1.3-2.1 1-2.9.7-.8 1.7-1.3 2.7-1.5 1.1-.1 2.1.2 3 .8l.3.2.3-.2c.8-.6 1.8-.9 2.8-.8 1.1.1 2 .5 2.7 1.3z"
+                style={{ fill: "none", stroke: "rgb(0, 0, 0)" }}
+              ></path>
+            </svg>
+          </button>
         </Box>
         <Box
           style={{
