0c1c3d46f3ea5cd82ca0ee6a14c30948f1b75088
rushikesh226 <rushikeshmali226@gmail.com>
2023-03-03 22:02:17 +0530

    main

--- sugar-cosmetics-clone/sugar/src/App.jsx
+++ sugar-cosmetics-clone/sugar/src/App.jsx
@@ -1,8 +1,6 @@
 import AllRoutes from "./Components/AllRoutes/AllRoutes";
 import { Navbar } from "./Components/Pages/aman/Navbar";
 import { Footer } from "./Components/Pages/aman/Footer";
-import { useContext } from "react";
-import { SortContext } from "./Components/Context/SortContext";
 function App() {
     return (
         <div className="App">
